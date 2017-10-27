6b1d01d33232f79a6c2af2998013f0ed5f02cb91
Eric Malamisura <eric.malamisura@gmail.com>
2017-10-26 21:29:26 -0400

    Initial Checking of Network
    Checking initial commit of Network

---
+++ stellar-dotnetcore-sdk/Network.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace stellar_dotnetcore_sdk
+{
+    public class Network
+    {
+        private static string PUBLIC = "Public Global Stellar Network ; September 2015";
+        private static string TESTNET = "Test SDF Network ; September 2015";
+
+        private static Network _current;
+
+        private static string _networkPassphrase;
+
+        public Network(string networkPassphrase)
+        {
+            if (!String.IsNullOrEmpty(networkPassphrase))
+                throw new ArgumentNullException(nameof(networkPassphrase));
+        }
+
+        public string NetworkPassphrase
+        {
+            get => _networkPassphrase;
+            private set => _networkPassphrase = value;
+        }
+
+        public byte[] NetworkId
+        {
+            //TODO: Implement
+            get;
+            set;
+        }
+
+        public static Network Current()
+        {
+            return _current;
+        }
+
+        public static void Use(Network network)
+        {
+            _current = network;
+        }
+
+        public static void UsePublicNetwork()
+        {
+            Use(new Network(PUBLIC));
+        }
+
+        public static void UseTestNetwork()
+        {
+            Use(new Network(TESTNET));
+        }
+    }
+}
---
+++ stellar-dotnetcore-unittest/NetworkTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace stellar_dotnetcore_unittest
+{
+    [TestClass]
+    public class NetworkTest
+    {
+        [TestMethod]
+        public void ResetNetwork()
+        {
+        }
+    }
+}
