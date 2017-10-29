be166fc949ffad8d436f4d85e4488ce881a2a153
Michael Monte <michaeljmonte@gmail.com>
2017-10-29 16:37:13 -0400

    Added AccountFlagTest

---
+++ stellar-dotnetcore-sdk/AccountFlag.cs
@@ -0,0 +1,14 @@
+﻿using stellar_dotnetcore_sdk.xdr;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnetcore_sdk
+{
+    public enum AccountFlag
+    {
+        AUTH_REQUIRED_FLAG = AccountFlags.AccountFlagsEnum.AUTH_REQUIRED_FLAG,
+        AUTH_REVOCABLE_FLAG = AccountFlags.AccountFlagsEnum.AUTH_REVOCABLE_FLAG,
+        AUTH_IMMUTABLE_FLAG = AccountFlags.AccountFlagsEnum.AUTH_IMMUTABLE_FLAG
+    }
+}
---
+++ stellar-dotnetcore-unittest/AccountFlagTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stellar_dotnetcore_sdk;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnetcore_unittest
+{
+    [TestClass]
+    public class AccountFlagTest
+    {
+        [TestMethod]
+        public void testValues()
+        {
+            Assert.AreEqual(1, (int)AccountFlag.AUTH_REQUIRED_FLAG);
+            Assert.AreEqual(2, (int)AccountFlag.AUTH_REVOCABLE_FLAG);
+            Assert.AreEqual(4, (int)AccountFlag.AUTH_IMMUTABLE_FLAG);
+        }
+    }
+}
