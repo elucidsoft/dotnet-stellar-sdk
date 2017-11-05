6cc4e1ee4799d190f72fa5c8cac0f9cb31a01f04
Michael Monte <michaeljmonte@gmail.com>
2017-11-05 18:25:51 -0500

    Fixes some naming violations

--- stellar-dotnetcore-sdk/responses/TransactionResponse.cs
+++ stellar-dotnetcore-sdk/responses/TransactionResponse.cs
@@ -6,27 +6,27 @@ namespace stellar_dotnetcore_sdk.responses
     public class TransactionResponse
     {
         [JsonProperty(PropertyName = "hash")]
-        public string hash { get; private set; }
+        public string Hash { get; private set; }
         [JsonProperty(PropertyName = "ledger")]
-        public long ledger { get; private set; }
+        public long Ledger { get; private set; }
         [JsonProperty(PropertyName = "created_at")]
-        public string createdAt { get; private set; }
+        public string CreatedAt { get; private set; }
         [JsonProperty(PropertyName = "source_account")]
-        public KeyPair sourceAccount { get; private set; }
+        public KeyPair SourceAccount { get; private set; }
         [JsonProperty(PropertyName = "paging_token")]
-        public string pagingToken { get; private set; }
+        public string PagingToken { get; private set; }
         [JsonProperty(PropertyName = "source_account_sequence")]
-        public long sourceAccountSequence { get; private set; }
+        public long SourceAccountSequence { get; private set; }
         [JsonProperty(PropertyName = "fee_paid")]
-        public long feePaid { get; private set; }
+        public long FeePaid { get; private set; }
         [JsonProperty(PropertyName = "operation_count")]
-        public int operationCount { get; private set; }
+        public int OperationCount { get; private set; }
         [JsonProperty(PropertyName = "envelope_xdr")]
-        public string envelopeXdr { get; private set; }
+        public string EnvelopeXdr { get; private set; }
         [JsonProperty(PropertyName = "result_xdr")]
-        public string resultXdr { get; private set; }
+        public string ResultXdr { get; private set; }
         [JsonProperty(PropertyName = "result_meta_xdr")]
-        public string resultMetaXdr { get; private set; }
+        public string ResultMetaXdr { get; private set; }
         [JsonProperty(PropertyName = "_links")]
         public TransactionResponseLinks Links { get; private set; }
 
@@ -53,19 +53,19 @@ namespace stellar_dotnetcore_sdk.responses
 
         public TransactionResponse(string hash, long ledger, string createdAt, KeyPair sourceAccount, string pagingToken, long sourceAccountSequence, long feePaid, int operationCount, string envelopeXdr, string resultXdr, string resultMetaXdr, Memo memo, TransactionResponseLinks links)
         {
-            this.hash = hash;
-            this.ledger = ledger;
-            this.createdAt = createdAt;
-            this.sourceAccount = sourceAccount;
-            this.pagingToken = pagingToken;
-            this.sourceAccountSequence = sourceAccountSequence;
-            this.feePaid = feePaid;
-            this.operationCount = operationCount;
-            this.envelopeXdr = envelopeXdr;
-            this.resultXdr = resultXdr;
-            this.resultMetaXdr = resultMetaXdr;
-            this.Memo = memo;
-            this.Links = links;
+            Hash = hash;
+            Ledger = ledger;
+            CreatedAt = createdAt;
+            SourceAccount = sourceAccount;
+            PagingToken = pagingToken;
+            SourceAccountSequence = sourceAccountSequence;
+            FeePaid = feePaid;
+            OperationCount = operationCount;
+            EnvelopeXdr = envelopeXdr;
+            ResultXdr = resultXdr;
+            ResultMetaXdr = resultMetaXdr;
+            Memo = memo;
+            Links = links;
         }
 
        ///
@@ -74,29 +74,29 @@ namespace stellar_dotnetcore_sdk.responses
         public class TransactionResponseLinks
         {
             [JsonProperty(PropertyName = "account")]
-            public Link account { get; private set; }
+            public Link Account { get; private set; }
             [JsonProperty(PropertyName = "effects")]
-            public Link effects { get; private set; }
+            public Link Effects { get; private set; }
             [JsonProperty(PropertyName = "ledger")]
-            public Link ledger { get; private set; }
+            public Link Ledger { get; private set; }
             [JsonProperty(PropertyName = "operations")]
-            public Link operations { get; private set; }
+            public Link Operations { get; private set; }
             [JsonProperty(PropertyName = "precedes")]
-            public Link precedes { get; private set; }
+            public Link Precedes { get; private set; }
             [JsonProperty(PropertyName = "self")]
-            public Link self { get; private set; }
+            public Link Self { get; private set; }
             [JsonProperty(PropertyName = "succeeds")]
-            public Link succeeds { get; private set; }
+            public Link Succeeds { get; private set; }
 
             public TransactionResponseLinks(Link account, Link effects, Link ledger, Link operations, Link self, Link precedes, Link succeeds)
             {
-                this.account = account;
-                this.effects = effects;
-                this.ledger = ledger;
-                this.operations = operations;
-                this.self = self;
-                this.precedes = precedes;
-                this.succeeds = succeeds;
+                Account = account;
+                Effects = effects;
+                Ledger = ledger;
+                Operations = operations;
+                Self = self;
+                Precedes = precedes;
+                Succeeds = succeeds;
             }
 
         }
