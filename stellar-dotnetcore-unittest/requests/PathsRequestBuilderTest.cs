751a897137ebfa16689bd8813a4885be5b24b06c
Eric Malamisura <eric@elucidsoft.com>
2017-11-05 17:18:56 -0500

    PathRequestBuilderTests, all unit tests pass

---
+++ stellar-dotnetcore-unittest/requests/PathsRequestBuilderTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stellar_dotnetcore_sdk;
+using stellar_dotnetcore_sdk.requests;
+
+namespace stellar_dotnetcore_unittest.requests
+{
+    [TestClass]
+    public class PathsRequestBuilderTest
+    {
+
+        [TestMethod]
+        public void TestAccounts()
+        {
+            Server server = new Server("https://horizon-testnet.stellar.org");
+            Uri uri = server.Paths
+                .DestinationAccount(KeyPair.FromAccountId("GB24QI3BJNKBY4YNJZ2I37HFIYK56BL2OURFML76X46RQQKDLVT7WKJF"))
+                .SourceAccount(KeyPair.FromAccountId("GD4KO3IOYYWIYVI236Y35K2DU6VNYRH3BPNFJSH57J5BLLCQHBIOK3IN"))
+                .DestinationAmount("20.50")
+                .DestinationAsset(Asset.CreateNonNativeAsset("USD", KeyPair.FromAccountId("GAYSHLG75RPSMXWJ5KX7O7STE6RSZTD6NE4CTWAXFZYYVYIFRUVJIBJH")))
+                .Cursor("13537736921089")
+                .Limit(200)
+                .Order(OrderDirection.ASC)
+                .BuildUri();
+
+            Assert.AreEqual("https://horizon-testnet.stellar.org/paths?" +
+                         "destination_account=GB24QI3BJNKBY4YNJZ2I37HFIYK56BL2OURFML76X46RQQKDLVT7WKJF&" +
+                         "source_account=GD4KO3IOYYWIYVI236Y35K2DU6VNYRH3BPNFJSH57J5BLLCQHBIOK3IN&" +
+                         "destination_amount=20.50&" +
+                         "destination_asset_type=credit_alphanum4&" +
+                         "destination_asset_code=USD&" +
+                         "destination_asset_issuer=GAYSHLG75RPSMXWJ5KX7O7STE6RSZTD6NE4CTWAXFZYYVYIFRUVJIBJH&" +
+                         "cursor=13537736921089&" +
+                         "limit=200&" +
+                         "order=asc", uri.ToString());
+        }
+    }
+}
