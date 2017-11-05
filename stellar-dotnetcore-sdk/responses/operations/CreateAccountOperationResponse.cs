bb71eaab153cfaf831255521c729c3bcc6f29197
Eric Malamisura <eric@elucidsoft.com>
2017-11-05 16:24:27 -0500

    OperationDeserializerTests

--- stellar-dotnetcore-sdk/responses/operations/CreateAccountOperationResponse.cs
+++ stellar-dotnetcore-sdk/responses/operations/CreateAccountOperationResponse.cs
@@ -1,6 +1,29 @@
-﻿namespace stellar_dotnetcore_sdk.responses.operations
+﻿using Newtonsoft.Json;
+
+namespace stellar_dotnetcore_sdk.responses.operations
 {
+    /// <summary>
+    /// Represents CreateAccount operation response.
+    /// See: https://www.stellar.org/developers/horizon/reference/resources/operation.html
+    /// <seealso cref="OperationRequestBuilder"/>
+    /// <seealso cref="Server"/>
+    /// </summary>
     public class CreateAccountOperationResponse : OperationResponse
     {
+        [JsonProperty(PropertyName = "account")]
+        public KeyPair Account { get; private set; }
+
+        [JsonProperty(PropertyName = "funder")]
+        public KeyPair Funder { get; private set; }
+
+        [JsonProperty(PropertyName = "starting_balance")]
+        public string StartingBalance { get; private set; }
+
+        public CreateAccountOperationResponse(KeyPair account, KeyPair funder, string startingBalance)
+        {
+            Account = account;
+            Funder = funder;
+            StartingBalance = startingBalance;
+        }
     }
 }
--- stellar-dotnetcore-unittest/responses/OperationDeserializerTest.cs
+++ stellar-dotnetcore-unittest/responses/OperationDeserializerTest.cs
@@ -14,7 +14,23 @@ namespace stellar_dotnetcore_unittest.responses
             var json = File.ReadAllText(Path.Combine("responses", "testdata", "operationCreateAccount.json"));
             var operation = JsonSingleton.GetInstance<CreateAccountOperationResponse>(json);
 
+            Assert.AreEqual(operation.SourceAccount.AccountId, "GD6WU64OEP5C4LRBH6NK3MHYIA2ADN6K6II6EXPNVUR3ERBXT4AN4ACD");
+            Assert.AreEqual(operation.PagingToken, "3936840037961729");
+            Assert.AreEqual(operation.Id, 3936840037961729L);
 
+            Assert.AreEqual(operation.Account.AccountId, "GAR4DDXYNSN2CORG3XQFLAPWYKTUMLZYHYWV4Y2YJJ4JO6ZJFXMJD7PT");
+            Assert.AreEqual(operation.StartingBalance, "299454.904954");
+            Assert.AreEqual(operation.Funder.AccountId, "GD6WU64OEP5C4LRBH6NK3MHYIA2ADN6K6II6EXPNVUR3ERBXT4AN4ACD");
+
+            Assert.AreEqual(operation.Links.Effects.Href, "/operations/3936840037961729/effects{?cursor,limit,order}");
+            Assert.AreEqual(operation.Links.Precedes.Href, "/operations?cursor=3936840037961729&order=asc");
+            Assert.AreEqual(operation.Links.Self.Href, "/operations/3936840037961729");
+            Assert.AreEqual(operation.Links.Succeeds.Href, "/operations?cursor=3936840037961729&order=desc");
+            Assert.AreEqual(operation.Links.Transaction.Href, "/transactions/75608563ae63757ffc0650d84d1d13c0f3cd4970a294a2a6b43e3f454e0f9e6d");
         }
+
+        //TODO: TestDeserializePaymentOperation
+
+
     }
 }
