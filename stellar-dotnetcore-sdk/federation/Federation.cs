6f308ae36c2055fbbd707fe21d0a17ef96aa4812
Eric Malamisura <eric@elucidsoft.com>
2017-11-05 01:28:38 -0400

    Federation and FederationTest all unit tests pass

---
+++ stellar-dotnetcore-sdk/federation/Federation.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace stellar_dotnetcore_sdk.federation
+{
+    public static class Federation
+    {
+        public static async Task<FederationResponse> Resolve(string value)
+        {
+            var tokens = Regex.Split(value, "\\*");
+            if (tokens.Length == 1)
+                return new FederationResponse(null, value, null, null);
+
+            if (tokens.Length == 2)
+            {
+                var domain = tokens[1];
+                var server = await FederationServer.CreateForDomain(domain);
+                return await server.ResolveAddress(value);
+            }
+
+            throw new MalformedAddressException();
+        }
+    }
+}
---
+++ stellar-dotnetcore-unittest/federation/FederationTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Language;
+using stellar_dotnetcore_sdk.federation;
+
+namespace stellar_dotnetcore_unittest.federation
+{
+    [TestClass]
+    public class FederationTest
+    {
+        private const string StellarToml = "FEDERATION_SERVER = \"https://api.stellar.org/federation\"";
+        private const string SuccessResponse = "{\"stellar_address\":\"bob*stellar.org\",\"account_id\":\"GCW667JUHCOP5Y7KY6KGDHNPHFM4CS3FCBQ7QWDUALXTX3PGXLSOEALY\"}";
+
+        private const HttpStatusCode HttpOk = HttpStatusCode.OK;
+
+        private Mock<FakeHttpMessageHandler> _fakeHttpMessageHandler;
+        private HttpClient _httpClient;
+        private FederationServer _server;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _server = new FederationServer("https://api.stellar.org/federation", "stellar.org");
+
+            _fakeHttpMessageHandler = new Mock<FakeHttpMessageHandler> {CallBase = true};
+            _httpClient = new HttpClient(_fakeHttpMessageHandler.Object);
+            _server.HttpClient = _httpClient;
+        }
+
+        private ISetupSequentialResult<HttpResponseMessage> When()
+        {
+            return _fakeHttpMessageHandler.SetupSequence(a => a.Send(It.IsAny<HttpRequestMessage>()));
+        }
+
+        private HttpResponseMessage ResponseMessage(HttpStatusCode statusCode, string content)
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = statusCode,
+                Content = new StringContent(content)
+            };
+        }
+
+        [TestMethod]
+        public async Task TestResolveSuccess()
+        {
+            When()
+                .Returns(ResponseMessage(HttpOk, StellarToml))
+                .Returns(ResponseMessage(HttpOk, SuccessResponse));
+
+            var response = await Federation.Resolve("bob*stellar.org");
+            Assert.AreEqual(response.StellarAddress, "bob*stellar.org");
+            Assert.AreEqual(response.AccountId, "GCW667JUHCOP5Y7KY6KGDHNPHFM4CS3FCBQ7QWDUALXTX3PGXLSOEALY");
+            Assert.IsNull(response.MemoType);
+            Assert.IsNull(response.Memo);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MalformedAddressException))]
+        public async Task TestMalformedAddress()
+        {
+            var response = await Federation.Resolve("bob*stellar.org*test");
+        }
+    }
+}
