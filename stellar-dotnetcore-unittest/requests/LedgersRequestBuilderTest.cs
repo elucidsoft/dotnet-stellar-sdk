84b243665747d034fc1f9312794c4b60d6e6fe6f
Michael Monte <michaeljmonte@gmail.com>
2017-11-05 16:15:30 -0500

    Added LedgerRequestBuilderTest.

---
+++ stellar-dotnetcore-unittest/requests/LedgersRequestBuilderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stellar_dotnetcore_sdk;
+using stellar_dotnetcore_sdk.requests;
+using System;
+
+
+namespace stellar_dotnetcore_unittest.requests
+{
+    [TestClass]
+    public class LedgersRequestBuilderTest
+    {
+        [TestMethod]
+        public void TestAccounts()
+        {
+            Server server = new Server("https://horizon-testnet.stellar.org");
+            Uri uri = server.Ledgers
+                    .Limit(200)
+                    .Order(OrderDirection.ASC)
+                    .BuildUri();
+            Assert.AreEqual("https://horizon-testnet.stellar.org/ledgers?limit=200&order=asc", uri.ToString());
+        }
+    }
+}
