36f7cc117a5e1753093713b6acbeb9a8cb250674
Michael Monte <michaeljmonte@gmail.com>
2017-11-05 17:14:50 -0500

    Added OrderBookRequestBuilderTest

---
+++ stellar-dotnetcore-sdk/requests/OrderBookRequestBuilder.cs
@@ -0,0 +1,79 @@
+﻿using stellar_dotnetcore_sdk.responses;
+using stellar_dotnetcore_sdk.responses.page;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stellar_dotnetcore_sdk.requests
+{
+    public class OrderBookRequestBuilder : RequestBuilder<OrderBookRequestBuilder>
+    {
+        public OrderBookRequestBuilder(Uri serverURI) : base(serverURI, "order_book")
+        {
+           
+        }
+
+        public OrderBookRequestBuilder BuyingAsset(Asset asset)
+        { 
+            _uriBuilder.SetQueryParam("buying_asset_type", asset.GetType());
+            if (asset is AssetTypeCreditAlphaNum) {
+                AssetTypeCreditAlphaNum creditAlphaNumAsset = (AssetTypeCreditAlphaNum)asset;
+                _uriBuilder.SetQueryParam("buying_asset_code", creditAlphaNumAsset.Code);
+                _uriBuilder.SetQueryParam("buying_asset_issuer", creditAlphaNumAsset.Issuer.AccountId);
+            }
+            return this;
+        }
+
+        public OrderBookRequestBuilder SellingAsset(Asset asset)
+        {
+            _uriBuilder.SetQueryParam("selling_asset_type", asset.GetType());
+            if (asset is AssetTypeCreditAlphaNum) {
+                AssetTypeCreditAlphaNum creditAlphaNumAsset = (AssetTypeCreditAlphaNum)asset;
+                _uriBuilder.SetQueryParam("selling_asset_code", creditAlphaNumAsset.Code);
+                _uriBuilder.SetQueryParam("selling_asset_issuer", creditAlphaNumAsset.Issuer.AccountId);
+            }
+            return this;
+        }
+
+        ///<Summary>
+        /// Requests specific <code>uri</code> and returns {@link Page} of {@link EffectResponse}.
+        /// This method is helpful for getting the next set of results.
+        /// </Summary>
+        public static async Task<Page<OrderBookResponse>> Execute(Uri uri)
+        {
+            var responseHandler = new ResponseHandler<Page<OrderBookResponse>>();
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(uri);
+                return await responseHandler.HandleResponse(response);
+            }
+        }
+        ///<Summary>
+        /// Build and execute request.
+        /// </Summary>
+        public async Task<Page<OrderBookResponse>> Execute()
+        {
+            return await Execute(BuildUri());
+        }
+
+
+        public override RequestBuilder<OrderBookRequestBuilder> Cursor(String token)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override RequestBuilder<OrderBookRequestBuilder> Limit(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override RequestBuilder<OrderBookRequestBuilder> Order(OrderDirection direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ stellar-dotnetcore-sdk/responses/OrderBookResponse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace stellar_dotnetcore_sdk.responses
+{
+    public class OrderBookResponse
+    {
+        [JsonProperty(PropertyName = "base")]
+        public Asset OrderBookBase { get; private set; }
+        [JsonProperty(PropertyName = "counter")]
+        public Asset Counter { get; private set; }
+        [JsonProperty(PropertyName = "asks")]
+        public Row[] Asks { get; private set; }
+        [JsonProperty(PropertyName = "bids")]
+        public Row[] Bids { get; private set; }
+
+        public OrderBookResponse(Asset orderBookBase, Asset counter, Row[] asks, Row[] bids)
+        {
+            OrderBookBase = orderBookBase;
+            Counter = counter;
+            Asks = asks;
+            Bids = bids;
+        }
+
+
+        ///
+        /// Represents order book row.
+        ///
+        public class Row
+        {
+            [JsonProperty(PropertyName = "amount")]
+            public String Amount { get; private set; }
+            [JsonProperty(PropertyName = "price")]
+            public String Price { get; private set; }
+            [JsonProperty(PropertyName = "price_r")]
+            public Price PriceR { get; private set; }
+
+            public Row(String amount, String price, Price priceR)
+            {
+                this.Amount = amount ?? throw new ArgumentNullException(nameof(amount), "amount cannot be null");
+                this.Price = price ?? throw new ArgumentNullException(nameof(price), "price cannot be null");
+                this.PriceR = priceR ?? throw new ArgumentNullException(nameof(priceR), "priceR cannot be null");
+            }
+        }
+    }
+}
