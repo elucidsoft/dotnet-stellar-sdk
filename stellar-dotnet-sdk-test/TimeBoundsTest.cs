1c7184fd7155ed0780cfd3519a3062844816d843
Jai <devkirbyrawr@gmail.com>
2018-07-28 18:29:45 +0200

    Added TimeBoundsTest.cs class.

---
+++ stellar-dotnet-sdk-test/TimeBoundsTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Memo = stellar_dotnet_sdk.Memo;
+using TimeBounds = stellar_dotnet_sdk.TimeBounds;
+using Transaction = stellar_dotnet_sdk.Transaction;
+using XdrTransaction = stellar_dotnet_sdk.xdr.Transaction;
+
+namespace stellar_dotnet_sdk_test
+{
+    [TestClass]
+    public class TimeBoundsTest
+    {
+        [TestMethod]
+        public void TestTimeBoundsHashEquality()
+        {
+            var timeBounds = new TimeBounds(56, 65);
+            var timeBounds2 = new TimeBounds(56, 65);
+
+            Assert.AreEqual(timeBounds.GetHashCode(), timeBounds.GetHashCode());
+            Assert.AreEqual(timeBounds2, timeBounds2);
+        }
+    }
+}
