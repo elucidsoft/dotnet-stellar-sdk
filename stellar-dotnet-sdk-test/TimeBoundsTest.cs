7f24075810319398294eb86bbc1c93e08cac3ccf
Jai <devkirbyrawr@gmail.com>
2018-07-28 18:31:38 +0200

    Cleaning up usings of TimeBoundsTest.cs

--- stellar-dotnet-sdk-test/TimeBoundsTest.cs
+++ stellar-dotnet-sdk-test/TimeBoundsTest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Memo = stellar_dotnet_sdk.Memo;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TimeBounds = stellar_dotnet_sdk.TimeBounds;
-using Transaction = stellar_dotnet_sdk.Transaction;
-using XdrTransaction = stellar_dotnet_sdk.xdr.Transaction;
 
 namespace stellar_dotnet_sdk_test
 {
