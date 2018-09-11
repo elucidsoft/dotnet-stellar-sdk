65baa1e35486556edde891e0027ab5621efd61ba
Francesco Ceccon <francesco@ceccon.me>
2018-09-11 23:07:11 +0100

    Fix double increment of _pos when decoding DataValue
    When decoding DataValue, XdrDataInputStream would move _pos forward
    both ReadFixOpaque and Read, resulting in and IndexOutOfRange exception
    when decoding data with padding.

--- stellar-dotnet-sdk-test/xdr/XdrDataStreamTest.cs
+++ stellar-dotnet-sdk-test/xdr/XdrDataStreamTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using stellar_dotnet_sdk.xdr;
 
 namespace stellar_dotnet_sdk_test.xdr
@@ -39,6 +40,20 @@ namespace stellar_dotnet_sdk_test.xdr
         {
             var memo = "øûý™€♠♣♥†‡µ¢£€";
             Assert.AreEqual(memo, BackAndForthXdrStreaming(memo));
+        }
+
+        [TestMethod]
+        public void ReadFixedLengthOpaqueArray()
+        {
+            var bytes = new byte[] {1, 2, 3, 4, 5, 0, 0, 0, 1};
+            var xdrInputStream = new XdrDataInputStream(bytes);
+            var result = new byte[5];
+            xdrInputStream.Read(result, 0, 5);
+            var expected = new byte[] {1, 2, 3, 4, 5};
+            Assert.IsTrue(expected.SequenceEqual(result));
+
+            var sentinel = xdrInputStream.Read();
+            Assert.AreEqual(1, sentinel);
         }
     }
 }
