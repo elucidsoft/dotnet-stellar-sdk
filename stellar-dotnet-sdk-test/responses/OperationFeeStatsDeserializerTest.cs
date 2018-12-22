4c91a4c7499ebbe44357a98760891aa1c9369e1c
Francesco Ceccon <francesco@ceccon.me>
2018-12-22 12:07:52 +0000

    Fix OperationFeeStatsDeserializerTest

--- stellar-dotnet-sdk-test/responses/OperationFeeStatsDeserializerTest.cs
+++ stellar-dotnet-sdk-test/responses/OperationFeeStatsDeserializerTest.cs
@@ -4,18 +4,32 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using Newtonsoft.Json;
 using stellar_dotnet_sdk.responses;
 
 namespace stellar_dotnet_sdk_test.responses
 {
-    class OperationFeeStatsDeserializerTest
+    [TestClass]
+    public class OperationFeeStatsDeserializerTest
     {
         [TestMethod]
         public void TestDeserialize()
         {
-            var json = File.ReadAllText(Path.Combine("testdata", "operationPage.json"));
+            var json = File.ReadAllText(Path.Combine("testdata", "operationFeeStats.json"));
             var stats = JsonSingleton.GetInstance<OperationFeeStatsResponse>(json);
             AssertTestData(stats);
+        }
+
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var stats = new OperationFeeStatsResponse(101, 102, 100, 20882791);
+            var json = JsonConvert.SerializeObject(stats);
+            var back = JsonConvert.DeserializeObject<OperationFeeStatsResponse>(json);
+            Assert.AreEqual(stats.Min, back.Min);
+            Assert.AreEqual(stats.Mode, back.Mode);
+            Assert.AreEqual(stats.LastLedger, back.LastLedger);
+            Assert.AreEqual(stats.LastLedgerBaseFee, back.LastLedgerBaseFee);
         }
 
         public static void AssertTestData(OperationFeeStatsResponse stats)
