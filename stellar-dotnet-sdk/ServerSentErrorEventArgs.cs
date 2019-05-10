193ecd63e71758f22cbb14e0ad6d9603a3824a05
Eric Malamisura <eric@elucidsoft.com>
2019-05-09 21:27:32 -0400

    Some additional code cleanup work for EventSource.

---
+++ stellar-dotnet-sdk/ServerSentErrorEventArgs.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  Copyright 2014 Jonathan Bradshaw. All rights reserved.
+ *  Redistribution and use in source and binary forms, with or without modification, is permitted.
+ */
+
+using System;
+
+namespace stellar_dotnet_sdk
+{
+    public sealed partial class EventSource
+    {
+        /// <summary>
+        ///     Server Sent Error Event Object
+        /// </summary>
+        public sealed class ServerSentErrorEventArgs : EventArgs
+        {
+            /// <summary>
+            /// Internal Exception
+            /// </summary>
+            public Exception Exception { get; internal set; }
+        }
+    }
+}
