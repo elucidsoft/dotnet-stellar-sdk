bc5dca9aaceaa748f46f8938fa3e2d31995189e1
Francesco Ceccon <francesco@ceccon.me>
2019-05-13 22:24:34 +0100

    Replace chaos.nacl with NSec

--- stellar-dotnet-sdk/chaos.nacl/Internal/Ed25519Ref10/d2.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace stellar_dotnet_sdk.chaos.nacl.Internal.Ed25519Ref10
-{
-    internal static partial class LookupTables
-    {
-        internal static FieldElement d2 = new FieldElement(-21827239, -5839606, -30745221, 13898782, 229458, 15978800,
-            -12551817, -6495438, 29715968, 9444199);
-    }
-}
