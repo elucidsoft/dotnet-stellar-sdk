92f9c66f90d45065334cdd2621060679ebe9ea26
Francesco Ceccon <francesco@ceccon.me>
2020-02-06 21:23:06 +0000

    Update web authentication to SEP 10 v1.3.0

--- stellar-dotnet-sdk/WebAuthentication.cs
+++ stellar-dotnet-sdk/WebAuthentication.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace stellar_dotnet_sdk
@@ -71,6 +73,131 @@ namespace stellar_dotnet_sdk
         }
 
         /// <summary>
+        /// Read a SEP 10 challenge transaction and return the client account id.
+        ///
+        /// Performs the following checks:
+        ///
+        ///   1. Transaction sequence number is 0
+        ///   2. Transaction source account is <paramref name="serverAccountId"/>
+        ///   3. Transaction has one operation only, of type ManageDataOperation
+        ///   4. The ManageDataOperation name and value are correct
+        ///   5. Transaction time bounds are still valid
+        ///   6. Transaction is signed by server
+        /// </summary>
+        /// <param name="transaction">The challenge transaction</param>
+        /// <param name="serverAccountId">The server account id</param>
+        /// <param name="network">The network the transaction was submitted to, defaults to Network.Current</param>
+        /// <param name="now">Current time, defaults to DateTimeOffset.Now</param>
+        /// <returns>The client account id</returns>
+        /// <exception cref="InvalidWebAuthenticationException"></exception>
+        public static string ReadChallengeTransaction(Transaction transaction, string serverAccountId,
+            Network network = null, DateTimeOffset? now = null)
+        {
+            network = network ?? Network.Current;
+
+            if (transaction.SequenceNumber != 0)
+                throw new InvalidWebAuthenticationException("Challenge transaction sequence number must be 0");
+
+            if (transaction.SourceAccount.AccountId != serverAccountId)
+                throw new InvalidWebAuthenticationException("Challenge transaction source must be serverAccountId");
+
+            if (transaction.Operations.Length != 1)
+                throw new InvalidWebAuthenticationException("Challenge transaction must contain one operation");
+
+            var operation = transaction.Operations[0] as ManageDataOperation;
+
+            if (operation is null)
+                throw new InvalidWebAuthenticationException(
+                    "Challenge transaction operation must be of type ManageDataOperation");
+
+            if (operation.SourceAccount is null)
+                throw new InvalidWebAuthenticationException("Challenge transaction operation must have source account");
+
+            var clientAccountId = operation.SourceAccount.Address;
+
+            var stringValue = Encoding.UTF8.GetString(operation.Value);
+            if (stringValue.Length != 64)
+                throw new InvalidWebAuthenticationException(
+                    "Challenge transaction operation data must be 64 bytes long");
+
+            try
+            {
+                // There is no need to check for decoded value length since we know it's valid base64 and 64 bytes long.
+                var _ = Convert.FromBase64String(stringValue);
+            }
+            catch (System.FormatException)
+            {
+                throw new InvalidWebAuthenticationException(
+                    "Challenge transaction operation data must be base64 encoded");
+            }
+
+            if (!ValidateSignedBy(transaction, serverAccountId, network))
+                throw new InvalidWebAuthenticationException("Challenge transaction not signed by server");
+
+            if (!ValidateTimeBounds(transaction.TimeBounds, now ?? DateTimeOffset.Now))
+                throw new InvalidWebAuthenticationException("Challenge transaction expired");
+
+            return clientAccountId;
+        }
+
+        public static ICollection<string> VerifyChallengeTransactionThreshold(Transaction transaction,
+            string serverAccountId,
+            int threshold, Dictionary<string, int> signerSummary, Network network = null, DateTimeOffset? now = null)
+        {
+            var signersFound =
+                VerifyChallengeTransactionSigners(transaction, serverAccountId, signerSummary.Keys.ToArray(), network,
+                    now);
+            var weight = signersFound.Sum(signer => signerSummary[signer]);
+            if (weight < threshold)
+                throw new InvalidWebAuthenticationException(
+                    $"Signers with weight {weight} do not meet threshold {threshold}");
+            return signersFound;
+        }
+
+        /// <summary>
+        /// Verify that all signers of a SEP 10 transaction are accounted for.
+        ///
+        /// A transaction is verified if it signed by the server account, and all other signatures match the provided
+        /// signers. Additional signers can be provided that do not have a signature, but all signatures must be
+        /// matched to a signer for verification to succeed. If verification succeeds, the list of signers that were
+        /// found is returned, excluding the server account id.
+        /// </summary>
+        /// <param name="transaction">The challenge transaction</param>
+        /// <param name="serverAccountId">The server account id</param>
+        /// <param name="signers"></param>
+        /// <param name="network">The network the transaction was submitted to, defaults to Network.Current</param>
+        /// <param name="now">Current time, defaults to DateTimeOffset.Now</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string[] VerifyChallengeTransactionSigners(Transaction transaction, string serverAccountId,
+            ICollection<string> signers, Network network = null, DateTimeOffset? now = null)
+        {
+            if (!signers.Any())
+                throw new ArgumentException($"{nameof(signers)} must be non-empty");
+
+            network = network ?? Network.Current;
+
+            ReadChallengeTransaction(transaction, serverAccountId, network, now);
+
+            // Remove server signer if present
+            var serverKeypair = KeyPair.FromAccountId(serverAccountId);
+            var clientSigners = signers.Where(signer => signer != serverKeypair.Address).ToList();
+
+            var allSigners = clientSigners.Select(signer => signer.Clone()).ToList();
+            allSigners.Add(serverKeypair.Address);
+            var allSignersFound = VerifyTransactionSignatures(transaction, signers, network);
+
+            var serverSigner = allSignersFound.FirstOrDefault(signer => signer == serverKeypair.Address);
+            if (serverSigner is null)
+                throw new InvalidWebAuthenticationException("Challenge transaction not signed by server");
+
+            if (allSignersFound.Count != transaction.Signatures.Count)
+                throw new InvalidWebAuthenticationException("Challenge transaction has unrecognized signatures");
+
+            return allSignersFound.Where(signer => signer != serverSigner).ToArray();
+        }
+
+        /// <summary>
         /// Verify that a transaction is a valid Stellar Web Authentication transaction.
         ///
         /// Performs the following checks:
@@ -88,58 +215,24 @@ namespace stellar_dotnet_sdk
         /// <param name="now">Current time, defaults to DateTimeOffset.Now</param>
         /// <returns>True if the transaction is valid</returns>
         /// <exception cref="InvalidWebAuthenticationException"></exception>
+        [Obsolete("Use VerifyChallengeTransactionThreshold and VerifyChallengeTransactionSigners")]
         public static bool VerifyChallengeTransaction(Transaction transaction, string serverAccountId,
             Network network = null, DateTimeOffset? now = null)
         {
             network = network ?? Network.Current;
 
-            if (transaction.SequenceNumber != 0)
-                throw new InvalidWebAuthenticationException("Challenge transaction sequence number must be 0");
+            var clientAccountId = ReadChallengeTransaction(transaction, serverAccountId, network, now);
 
-            if (transaction.SourceAccount.AccountId != serverAccountId)
-                throw new InvalidWebAuthenticationException("Challenge transaction source must be serverAccountId");
-
-            if (transaction.Operations.Length != 1)
-                throw new InvalidWebAuthenticationException("Challenge transaction must contain one operation");
-
-            var operation = transaction.Operations[0] as ManageDataOperation;
-
-            if (operation is null)
-                throw new InvalidWebAuthenticationException("Challenge transaction operation must be of type ManageDataOperation");
-
-            var stringValue = Encoding.UTF8.GetString(operation.Value);
-            if (stringValue.Length != 64)
-                throw new InvalidWebAuthenticationException("Challenge transaction operation data must be 64 bytes long");
-
-            try
-            {
-                // There is no need to check for decoded value length since we know it's valid base64 and 64 bytes long.
-                var _ = Convert.FromBase64String(stringValue);
-            }
-            catch (System.FormatException)
-            {
-                throw new InvalidWebAuthenticationException("Challenge transaction operation data must be base64 encoded");
-            }
-
-            if (!ValidateSignedBy(transaction, serverAccountId, network))
-                throw new InvalidWebAuthenticationException("Challenge transaction not signed by server");
-
-            if (!ValidateSignedBy(transaction, operation.SourceAccount.AccountId, network))
+            if (!ValidateSignedBy(transaction, clientAccountId, network))
                 throw new InvalidWebAuthenticationException("Challenge transaction not signed by client");
-
-            if (!ValidateTimeBounds(transaction.TimeBounds, now ?? DateTimeOffset.Now))
-                throw new InvalidWebAuthenticationException("Challenge transaction expired");
 
             return true;
         }
 
         private static bool ValidateSignedBy(Transaction transaction, string accountId, Network network)
         {
-            var transactionHash = transaction.Hash(network);
-            var keypair = KeyPair.FromAccountId(accountId);
-
-            var signature = transaction.Signatures.Find(sig => keypair.Verify(transactionHash, sig.Signature));
-            return signature != null;
+            var signaturesUsed = VerifyTransactionSignatures(transaction, new[] {accountId}, network);
+            return signaturesUsed.Count == 1;
         }
 
         private static bool ValidateTimeBounds(TimeBounds timeBounds, DateTimeOffset now)
@@ -148,6 +241,33 @@ namespace stellar_dotnet_sdk
             if (timeBounds.MinTime == 0 || timeBounds.MaxTime == 0) return false;
             var unixNow = now.ToUnixTimeSeconds();
             return timeBounds.MinTime <= unixNow && unixNow <= timeBounds.MaxTime;
+        }
+
+        private static ICollection<string> VerifyTransactionSignatures(Transaction transaction,
+            IEnumerable<string> signers, Network network)
+        {
+            var txHash = transaction.Hash(network);
+            var signaturesUsed = new Dictionary<xdr.DecoratedSignature, string>();
+
+            foreach (var signer in signers)
+            {
+                var keypair = KeyPair.FromAccountId(signer);
+                foreach (var signature in transaction.Signatures)
+                {
+                    if (signaturesUsed.ContainsKey(signature))
+                        continue;
+
+                    if (!signature.Hint.InnerValue.SequenceEqual(keypair.SignatureHint.InnerValue))
+                        continue;
+
+                    if (keypair.Verify(txHash, signature.Signature))
+                    {
+                        signaturesUsed[signature] = keypair.Address;
+                    }
+                }
+            }
+
+            return signaturesUsed.Values.ToArray();
         }
     }
 }
