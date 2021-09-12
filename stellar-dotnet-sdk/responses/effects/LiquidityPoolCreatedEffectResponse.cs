f4e614c5336f20178e865c96552a12e18891ddb2
Kirbyrawr <devkirbyrawr@gmail.com>
2021-09-12 17:50:48 +0200

    Create LiquidityPoolCreatedEffectResponse.cs

---
+++ stellar-dotnet-sdk/responses/effects/LiquidityPoolCreatedEffectResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnet_sdk.responses.effects
+{
+    public class LiquidityPoolCreatedEffectResponse : EffectResponse
+    {
+        public override int TypeId => 84;
+
+        [JsonProperty(PropertyName = "liquidity_pool")]
+        public LiquidityPoolEffectResponse LiquidityPool { get; set; }
+
+        public LiquidityPoolCreatedEffectResponse()
+        {
+
+        }
+
+        public LiquidityPoolCreatedEffectResponse(LiquidityPoolEffectResponse liquidityPool)
+        {
+            LiquidityPool = liquidityPool;
+        }
+    }
+}
