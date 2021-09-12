5b480bfa954e8e0161919fd929af8275eb2e4daf
Kirbyrawr <devkirbyrawr@gmail.com>
2021-09-12 17:51:03 +0200

    Create LiquidityPoolClaimableAssetAmount.cs

---
+++ stellar-dotnet-sdk/LiquidityPoolClaimableAssetAmount.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnet_sdk
+{
+    public class LiquidityPoolClaimableAssetAmount
+    {
+        [JsonProperty(PropertyName = "asset")]
+        public string Asset { get; set; }
+
+        [JsonProperty(PropertyName = "amount")]
+        public string Amount { get; set; }
+
+        [JsonProperty(PropertyName ="claimable_balance_id")]
+        public string ClaimableBalanceID { get; set; }
+
+        public LiquidityPoolClaimableAssetAmount(string asset, string amount, string claimableBalanceID)
+        {
+            Asset = asset;
+            Amount = amount;
+            ClaimableBalanceID = claimableBalanceID;
+        }
+    }
+}
