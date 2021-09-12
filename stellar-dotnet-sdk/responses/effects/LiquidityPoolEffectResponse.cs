a3de141713651d5e338ab0dcca89c99aa6852915
Kirbyrawr <devkirbyrawr@gmail.com>
2021-09-12 17:50:52 +0200

    Create LiquidityPoolEffectResponse.cs

---
+++ stellar-dotnet-sdk/responses/effects/LiquidityPoolEffectResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnet_sdk.responses.effects
+{
+    public class LiquidityPoolEffectResponse : Response
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "fee_bp")]
+        public int FeeBP { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "total_trustlines")]
+        public string TotalTrustlines { get; set; }
+
+        [JsonProperty(PropertyName = "total_shares")]
+        public string TotalShares { get; set; }
+
+        [JsonProperty(PropertyName = "reserves")]
+        public AssetAmount[] Reserves { get; set; }
+    }
+}
