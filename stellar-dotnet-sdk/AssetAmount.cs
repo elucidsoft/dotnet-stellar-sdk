9998f735d6d0c6e857e29519a06311157c746113
Kirbyrawr <devkirbyrawr@gmail.com>
2021-09-12 17:51:05 +0200

    Create AssetAmount.cs

---
+++ stellar-dotnet-sdk/AssetAmount.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stellar_dotnet_sdk
+{
+    public class AssetAmount
+    {
+        [JsonProperty(PropertyName = "asset")]
+        public string Asset { get; set; }
+
+        [JsonProperty(PropertyName = "amount")]
+        public string Amount { get; set; }
+
+        public AssetAmount (string asset, string amount)
+        {
+            Asset = asset;
+            Amount = amount;
+        }
+    }
+}
