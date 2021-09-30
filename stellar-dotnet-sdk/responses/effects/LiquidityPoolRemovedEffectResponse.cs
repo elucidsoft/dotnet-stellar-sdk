916305756cbb61848edd471a3e39e9326cab4ee6
Kirbyrawr <devkirbyrawr@gmail.com>
2021-09-30 23:16:37 +0200

    Added LiquidityPoolRemovedEffectResponse tests

--- stellar-dotnet-sdk/responses/effects/LiquidityPoolRemovedEffectResponse.cs
+++ stellar-dotnet-sdk/responses/effects/LiquidityPoolRemovedEffectResponse.cs
@@ -10,11 +10,11 @@ namespace stellar_dotnet_sdk.responses.effects
         public override int TypeId => 94;
 
         [JsonProperty(PropertyName = "liquidity_pool_id")]
-        public LiquidityPool LiquidityPoolID { get; }
+        public LiquidityPoolID LiquidityPoolID { get; set; }
 
         public LiquidityPoolRemovedEffectResponse() { }
 
-        public LiquidityPoolRemovedEffectResponse(LiquidityPool liquidityPoolID)
+        public LiquidityPoolRemovedEffectResponse(LiquidityPoolID liquidityPoolID)
         {
             LiquidityPoolID = liquidityPoolID;
         }
