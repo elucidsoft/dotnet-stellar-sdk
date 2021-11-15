01eac839e47b32c07bfc0eafa71e932e5116dc41
Jerome Bell <jeromebell@gmail.com>
2021-11-14 18:31:49 -0600

    Added deserialization of auth_clawback_enabled flag

--- stellar-dotnet-sdk/responses/Flags.cs
+++ stellar-dotnet-sdk/responses/Flags.cs
@@ -7,20 +7,36 @@ namespace stellar_dotnet_sdk.responses
     /// </summary>
     public class Flags
     {
-        public Flags(bool authRequired, bool authRevocable, bool authImmutable)
+        public Flags(bool authRequired, bool authRevocable, bool authImmutable, bool authClawback)
         {
             AuthRequired = authRequired;
             AuthRevocable = authRevocable;
             AuthImmutable = authImmutable;
+            AuthClawback = authClawback;
         }
 
+        /// <summary>
+        /// This account must approve anyone who wants to hold its asset.
+        /// </summary>
         [JsonProperty(PropertyName = "auth_required")]
         public bool AuthRequired { get; private set; }
 
+        /// <summary>
+        /// This account can set the authorize flag of an existing trustline to freeze the assets held by an asset holder.
+        /// </summary>
         [JsonProperty(PropertyName = "auth_revocable")]
         public bool AuthRevocable { get; private set; }
 
+        /// <summary>
+        /// This account cannot change any of the authorization flags.
+        /// </summary>
         [JsonProperty(PropertyName = "auth_immutable")]
         public bool AuthImmutable { get; private set; }
+
+        /// <summary>
+        /// This account can unilaterally take away any portion of its issued asset(s) from any asset holders.
+        /// </summary>
+        [JsonProperty(PropertyName = "auth_clawback_enabled")]
+        public bool AuthClawback { get; private set; }
     }
 }
