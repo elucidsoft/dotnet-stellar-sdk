a70e58ee1d285e44bc2d234e1eb351e88c896ee6
Kirbyrawr <devkirbyrawr@gmail.com>
2022-04-26 23:25:13 +0200

    Added LedgerBounds and TransactionPreconditions

---
+++ stellar-dotnet-sdk/LedgerBounds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xdr = stellar_dotnet_sdk.xdr;
+
+namespace stellar_dotnet_sdk
+{
+    /// <summary>
+    /// LedgerBounds are Preconditions of a transaction per<a href="https://github.com/stellar/stellar-protocol/blob/master/core/cap-0021.md#specification">CAP-21<a/>
+    /// </summary>
+    public class LedgerBounds
+    {
+        private readonly uint _minLedger;
+        private readonly uint _maxLedger;
+
+        public uint MinLedger => _minLedger;
+        public uint MaxLedger => _maxLedger;
+
+        public LedgerBounds(uint minLedger, uint maxLedger)
+        {
+            _minLedger = minLedger;
+            _maxLedger = maxLedger;
+        }
+
+        public static LedgerBounds FromXdr(xdr.LedgerBounds xdrLedgerBounds)
+        {
+            return new LedgerBounds(xdrLedgerBounds.MinLedger.InnerValue, xdrLedgerBounds.MaxLedger.InnerValue);
+        }
+
+        public xdr.LedgerBounds ToXdr()
+        {
+            return new xdr.LedgerBounds()
+            {
+                MinLedger = new xdr.Uint32() { InnerValue = _minLedger },
+                MaxLedger = new xdr.Uint32() { InnerValue = _maxLedger }
+            };
+        }
+    }
+}
---
+++ stellar-dotnet-sdk/TransactionPreconditions.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stellar_dotnet_sdk
+{
+    public class TransactionPreconditions
+    {
+        public static long MAX_EXTRA_SIGNERS_COUNT = 2;
+        public static long TIMEOUT_INFINITE = 0;
+
+        public LedgerBounds LedgerBounds { get; private set; }
+        public ulong MinSeqAge { get; private set; }
+        public long? MinSeqNumber { get; private set; }
+        public uint MinSeqLedgerGap { get; private set; }
+        public List<xdr.SignerKey> ExtraSigners { get; private set; }
+        public TimeBounds TimeBounds { get; private set; }
+
+        public void IsValid()
+        {
+            if (TimeBounds == null)
+            {
+                throw new FormatException("Invalid preconditions, must define timebounds");
+            }
+
+            if (ExtraSigners.Count > MAX_EXTRA_SIGNERS_COUNT)
+            {
+                throw new FormatException("Invalid preconditions, too many extra signers, can only have up to " + MAX_EXTRA_SIGNERS_COUNT);
+            }
+        }
+
+        public bool HasV2()
+        {
+            return (LedgerBounds != null ||
+                    (MinSeqLedgerGap > 0) ||
+                    (MinSeqAge > 0) ||
+                    MinSeqNumber != null ||
+                    ExtraSigners?.Count != 0);
+        }
+
+        public static TransactionPreconditions FromXDR(xdr.Preconditions preconditions)
+        {
+            TransactionPreconditions transactionPreconditions = new TransactionPreconditions();
+
+            if (preconditions.Discriminant.InnerValue == xdr.PreconditionType.PreconditionTypeEnum.PRECOND_V2)
+            {
+                //Time Bounds
+                if (preconditions.V2.TimeBounds != null)
+                {
+                    transactionPreconditions.TimeBounds = new TimeBounds(preconditions.V2.TimeBounds.MinTime.InnerValue.InnerValue, preconditions.V2.TimeBounds.MaxTime.InnerValue.InnerValue);
+                }
+
+                //Extra Signers
+                if (preconditions.V2.ExtraSigners != null && preconditions.V2.ExtraSigners.Length > 0)
+                {
+                    transactionPreconditions.ExtraSigners = preconditions.V2.ExtraSigners.ToList();
+                }
+
+                //Min Seq Age
+                if (preconditions.V2.MinSeqAge != null)
+                {
+                    transactionPreconditions.MinSeqAge = preconditions.V2.MinSeqAge.InnerValue.InnerValue;
+                }
+
+                //Ledger Bounds
+                if (preconditions.V2.LedgerBounds != null)
+                {
+                    transactionPreconditions.LedgerBounds = LedgerBounds.FromXdr(preconditions.V2.LedgerBounds);
+                }
+
+                //Min Seq Num
+                if (preconditions.V2.MinSeqNum != null)
+                {
+                    transactionPreconditions.MinSeqNumber = preconditions.V2.MinSeqNum.InnerValue.InnerValue;
+                }
+
+                //Min Seq Ledger Gap
+                if (preconditions.V2.MinSeqLedgerGap != null)
+                {
+                    transactionPreconditions.MinSeqLedgerGap = preconditions.V2.MinSeqLedgerGap.InnerValue;
+                }
+            }
+            else
+            {
+                if(preconditions.TimeBounds != null)
+                {
+                    transactionPreconditions.TimeBounds = new TimeBounds(preconditions.TimeBounds.MinTime.InnerValue.InnerValue, preconditions.TimeBounds.MaxTime.InnerValue.InnerValue);
+                }
+            }
+
+            return transactionPreconditions;
+        }
+
+        public xdr.Preconditions ToXDR()
+        {
+            var preconditions = new xdr.Preconditions();
+
+            if(HasV2())
+            {
+                preconditions.Discriminant.InnerValue = xdr.PreconditionType.PreconditionTypeEnum.PRECOND_V2;
+                
+                var preconditionsV2 = new xdr.PreconditionsV2();
+
+                preconditionsV2.ExtraSigners = ExtraSigners == null ? new xdr.SignerKey[] { new xdr.SignerKey() } : ExtraSigners.ToArray();
+
+                if (LedgerBounds != null)
+                {
+                    var ledgerBoundsXDR = new xdr.LedgerBounds();
+                    ledgerBoundsXDR.MinLedger = new xdr.Uint32(LedgerBounds.MinLedger);
+                    ledgerBoundsXDR.MaxLedger = new xdr.Uint32(LedgerBounds.MaxLedger);
+
+                    preconditionsV2.LedgerBounds = ledgerBoundsXDR;
+                }
+
+                if(MinSeqNumber != null)
+                {
+                    preconditionsV2.MinSeqNum = new xdr.SequenceNumber(new xdr.Int64(MinSeqNumber.Value));
+                }
+
+                preconditionsV2.MinSeqLedgerGap = new xdr.Uint32(MinSeqLedgerGap);
+
+                if(TimeBounds != null)
+                {
+                    preconditionsV2.TimeBounds = TimeBounds.ToXdr();
+                }
+            }
+            else
+            {
+                if(TimeBounds == null)
+                {
+                    preconditions.Discriminant.InnerValue = xdr.PreconditionType.PreconditionTypeEnum.PRECOND_NONE;
+                }
+                else
+                {
+                    preconditions.Discriminant.InnerValue = xdr.PreconditionType.PreconditionTypeEnum.PRECOND_TIME;
+                    preconditions.TimeBounds = TimeBounds.ToXdr();
+                }
+            }
+
+            return preconditions;
+        }
+    }
+}
