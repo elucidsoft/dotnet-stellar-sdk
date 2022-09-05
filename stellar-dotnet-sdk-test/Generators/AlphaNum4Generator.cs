c6d8941284851beedb1a98568076c864797b8463
Jerome Bell <jeromebell@gmail.com>
2022-09-04 22:52:13 -0500

    Fix whitespace errors.

--- stellar-dotnet-sdk-test/Generators/AlphaNum4Generator.cs
+++ stellar-dotnet-sdk-test/Generators/AlphaNum4Generator.cs
@@ -13,11 +13,11 @@ namespace stellar_dotnet_sdk_test.Generators
             var lowerCaseLetter = Gen.Choose('a', 'z');
             var upperCaseLetter = Gen.Choose('A', 'Z');
             var number = Gen.Choose('0', '9');
-            var validCharacter = 
+            var validCharacter =
                 Gen.OneOf(lowerCaseLetter, upperCaseLetter, number)
                     .Select(character => (char)character);
 
-            return 
+            return
                 codeLength
                     .SelectMany(length => Gen.ListOf(length, validCharacter))
                     .Select(characters => new string(characters.ToArray()))
